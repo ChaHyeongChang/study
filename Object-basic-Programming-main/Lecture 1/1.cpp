f845422ce4fa1246b30ddb11b3817bd5e1c32603
ChaHyeongChang <56flower1@naver.com>
2025-05-09 09:29:14 +0900

    05.09

--- Object-basic-Programming-main/Lecture 1/1.cpp
+++ Object-basic-Programming-main/Lecture 1/1.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main(){
+    char cstr[50];
+    string str;
+
+    cin>>cstr;
+    getline(cin,str);
+
+    cout<<"C-Style:"<<cstr<< endl;
+    cout<<"C++Style:"<<str<< endl;
+}
--- Object-basic-Programming-main/Lecture 4/4-4.cpp
+++ Object-basic-Programming-main/Lecture 4/4-4.cpp
@@ -1,4 +1,4 @@
-/*
+
 #include <iostream>
 using namespace std;
 
@@ -21,7 +21,7 @@ int main() {
         
     cout << endl;
 }
-*/
+
 /*
 #include <iostream>
 using namespace std;
@@ -45,28 +45,6 @@ int main() {
         
     cout << endl;
 }
-    */
+    
 
-
-
-#include <iostream>
-using namespace std;
-
-template <class T>
-void reverseArray(T *a, int size){
-    T temp[size];
-    for(int i = 0; i < size; i++){
-        
-    }
-}
-
-int main() {
-    int x[] = {1, 10, 100, 5, 4};
-    reverseArray(x, 5);
-
-    for(int i=0; i<5; i++){
-        cout << x[i] << ' '; // 4 5 100 10 1 이 출력된다.
-    }
-        
-    cout << endl;
-}
+*/
